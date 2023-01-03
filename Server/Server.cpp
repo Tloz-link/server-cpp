a89742e2ff74b8843dcbd82a76bde5425942cda1
Tloz <dndud088@naver.com>
2023-01-03 13:19:47 +0900

    초기 커밋.

---
+++ Server/Server.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <windows.h>
+#include <atomic>
+#include <mutex>
+#include <map>
+#include <queue>
+using namespace std;
+#include "ThreadManager.h"
+
+void TestThread()
+{
+	cout << "Hi! i am thread " << LThreadId << endl;
+
+	while (true)
+	{
+
+	}
+}
+
+int main()
+{
+	for (int32 i = 0; i < 10; ++i)
+		GThreadManager->Launch(TestThread);
+
+	GThreadManager->Join();
+}
---
+++ Server/pch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+#include "CorePch.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "x64\\Debug\\ServerCore.lib")
+#else
+#pragma comment(lib, "x64\\Release\\ServerCore.lib")
+#endif
