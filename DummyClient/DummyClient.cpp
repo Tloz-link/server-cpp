72fa0441e834a87a60fba19651158b810dd43c1a
Tloz <dndud088@naver.com>
2023-01-09 16:12:59 +0900

    서버, 클라이언트 연결

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -1,8 +1,51 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+// 클라
+// 1) 소켓 생성 (socket)
+// 2) 서버에 연결 요청 (connect)
+// 3) 통신
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	WSADATA wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+		return 0;
+
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777);
+
+	if(::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		return 0;
+
+	cout << "Connected To Server!" << endl;
+
+	while (true)
+	{
+		// send는 서버에서 recv를 안해도 블로킹되지 않음
+		// 클라의 커널 레벨 sendBuffer에 복사 해둔 뒤 서버의 커널 레벨 recvBuffer에 전송
+		char sendBuffer[100] = "Hello I am Client!";
+		int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+		if (resultCode == SOCKET_ERROR)
+			return 0;
+
+		char recvBuffer[100];
+		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+		if (recvLen <= 0)
+			return 0;
+
+		cout << "Echo Data : " << recvBuffer << endl;
+
+		this_thread::sleep_for(1s);
+	}
+
+	::closesocket(clientSocket);
+	::WSACleanup();
 }
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -2,28 +2,76 @@
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <windows.h>
 #include <atomic>
 #include <mutex>
-#include <map>
-#include <queue>
 using namespace std;
-#include "ThreadManager.h"
+#include <windows.h>
 
-void TestThread()
-{
-	cout << "Hi! i am thread " << LThreadId << endl;
-
-	while (true)
-	{
-
-	}
-}
+// 서버
+// 1) 새로운 소켓 생성 (socket)
+// 2) 소켓에 주소/포트 번호 설정 (bind)
+// 3) 소켓 일 시키기 (listen)
+// 4) 손님 접속 (accept)
+// 5) 클라와 통신
 
 int main()
 {
-	for (int32 i = 0; i < 10; ++i)
-		GThreadManager->Launch(TestThread);
+	WSADATA wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
-	GThreadManager->Join();
+	//1) 소켓 생성
+	//int32 errorCode = ::WSAGetLastError();
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+		return 0;
+
+	// 2) 주소/포트 번호 설정
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+	serverAddr.sin_port = ::htons(7777);
+
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		return 0;
+
+	// 3) 업무 개시
+	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
+		return 0;
+
+	// 4)
+	while (true)
+	{
+		SOCKADDR_IN clientAddr;
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int addrLen = sizeof(clientAddr);
+
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+			return 0;
+
+		char ip[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ip, sizeof(ip));
+		cout << "Client Connected! IP = " << ip << endl;
+
+		while (true)
+		{
+			// 5)
+			char recvBuffer[100];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen <= 0)
+				return 0;
+
+			cout << "Recv Data : " << recvBuffer << endl;
+			cout << "Recv Data Len : " << recvLen << endl;
+
+			int32 resultCode = ::send(clientSocket, recvBuffer, recvLen, 0);
+			if (resultCode == SOCKET_ERROR)
+				return 0;
+		}
+	}
+
+	::closesocket(listenSocket);
+	::WSACleanup();
 }
