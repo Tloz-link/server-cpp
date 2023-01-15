1ededd5fab0c979c3dc843f4f7751b0f21255aa8
Tloz <dndud088@naver.com>
2023-01-15 11:43:05 +0900

    서버 IO 패턴 실습

---
+++ Study/IOCP.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <vector>
+using namespace std;
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+
+const int32 BUFSIZE = 1000;
+
+struct Session
+{
+	SOCKET socket = INVALID_SOCKET;
+	char recvBuffer[BUFSIZE] = {};
+	int32 recvBytes = 0;
+};
+
+int IOCP()
+{
+	SocketUtils::Init();
+
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+		return 0;
+
+	SocketUtils::SetReuseAddress(listenSocket, true);
+
+	if (SocketUtils::BindAnyAddress(listenSocket, 7777) == false)
+		return 0;
+
+	if (SocketUtils::Listen(listenSocket) == false)
+		return 0;
+
+	// IOCP 실습 필요 -> Overlapped 구조체, IO_TYPE enum 생성필요
+
+	SocketUtils::Close(listenSocket);
+}
---
+++ Study/pch.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "CorePch.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "x64\\Debug\\ServerCore.lib")
+#else
+#pragma comment(lib, "x64\\Release\\ServerCore.lib")
+#endif
